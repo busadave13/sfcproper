cfcfdc8d11f6c0ff4adec759d852d1b7cf33efed
Dave Harding <daveharding@outlook.com>
2018-12-09 08:22:47 -0800

    Add new backend

---
+++ src/BackEnd/Controllers/BackEndController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BackEnd.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BackEndController : ControllerBase
+    {
+        // GET api/values/5
+        [HttpGet("node")]
+        public ActionResult<string> Get()
+        {
+            return Environment.MachineName;
+        }
+    }
+}
